70d8eae3fb5ce72b6f92a5b779e0da8c7eb0a495
Adalberto Silvestre <157027383+AdalbertoAo@users.noreply.github.com>
2026-02-19 11:51:39 +0000

    difinido estilizacao de do codigo e o editor

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 export default function Home() {
     return (
-    <h1>V🤎 <br/> eu te amo muito, se viste essa paguina me diz alguma coisa</h1>
-    
-)
-    
+        <h1>V🤎 <br /> eu te amo muito, se viste essa paguina me diz alguma coisa</h1>
+    )
+
 }
